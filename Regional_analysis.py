1fa63cb12f66f9e80e6e5c60f7f15c934b97b35d
Pir Mohammad <74357667+pirmohammad291@users.noreply.github.com>
2024-11-13 15:26:50 +0800

    Regional_analysis.py

--- Regional_analysis.py
+++ Regional_analysis.py
@@ -8,11 +8,6 @@ import numpy as np
 
 book = Workbook()
 sheet1 = book.add_sheet("Raw")
-##sheet1.write(0,0, "File Name")
-##sheet1.write(0,1, 'Min') 
-##sheet1.write(0,2, 'Max')
-##sheet1.write(0,1, 'Mean')
-##sheet1.write(0,4, 'STD')
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.overwriteOutput = True
 InputFolder = r"C:\Users\pirmo\OneDrive - The Hong Kong Polytechnic University\LUMIP\land-noLu\tas\Mean_Model\\"
@@ -38,11 +33,6 @@ for city, filename in zip(cities, filenames):
     cityOutputPath = os.path.join(OutputFolder, filename)   # Current folder where results should be saved   
     if not os.path.isdir(cityOutputPath):
         os.mkdir(cityOutputPath)              # If output folder doesnt exist, create        
-##    extent = arcpy.Describe(city).extent
-##    west = extent.XMin
-##    south = extent.YMin
-##    east = extent.XMax
-##    north = extent.YMax
     print ("[+] City: "+filename)
     sheet1.write(0,col, filename)
     ex_row = 1
@@ -50,15 +40,9 @@ for city, filename in zip(cities, filenames):
             rasterName = raster.split("\\")[-1]
             if not os.path.exists(os.path.join(cityOutputPath, filename+"_"+rasterName)):
                 cityOutput = os.path.join(cityOutputPath, rasterName)
-##              nodata = arcpy.Describe(arcpy.Raster(raster)).noDataValue
                 print ("[+] Clipping: " + rasterName)
-##              arcpy.Clip_management(raster, "{} {} {} {}".format(west, south, east, north) , cityOutput, city, nodata, "ClippingGeometry", "NO_MAINTAIN_EXTENT")
                 arcpy.gp.ExtractByMask_sa(raster, city, cityOutput)
-##              sheet1.write(ex_row, 0, rasterName)
-##              sheet1.write(ex_row, 1, arcpy.GetRasterProperties_management(raster, "MINIMUM", "").getOutput(0))
-##              sheet1.write(ex_row, 2, arcpy.GetRasterProperties_management(raster, "MAXIMUM", "").getOutput(0))
                 sheet1.write(ex_row, col, arcpy.GetRasterProperties_management(cityOutput, "MEAN", "").getOutput(0))
-##              sheet1.write(ex_row, 4, arcpy.GetRasterProperties_management(raster, "STD", "").getOutput(0))
             ex_row = ex_row + 1
     col = col + 1          
 book.save (OutputFolder+"Stats.xls")
